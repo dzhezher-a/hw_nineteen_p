5b4b28f199b8328c2b061b998949980072e8586e
dzhezher-a <121169002+dzhezher-a@users.noreply.github.com>
2023-06-12 20:11:07 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const btnOne = document.querySelector(".https-check-btn-one");
+const btnTwo = document.querySelector(".https-check-btn-two");
+
+function checkProtocol() {
+  let url = "github.com/dzhezher-a/hw_nineteen_p";
+  const httpsRegex = new RegExp(/^https?:\/\//);
+  if (!httpsRegex.test()) {
+    url = "https://" + url;
+  }
+
+  window.location.open(url, "_blank");
+}
+
+btnOne.addEventListener("click", checkProtocol);
+btnTwo.addEventListener("click", checkProtocol);
